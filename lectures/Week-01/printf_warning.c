b15c03d9e84875da75fcdefe3882c46a8a7e11c7
Paul Miller <paul.miller@rmit.edu.au>
2018-11-26 18:50:20 +1100

    reset the printf_warning program back to being incorrect

--- lectures/Week-01/printf_warning.c
+++ lectures/Week-01/printf_warning.c
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-    int i=5;
-    char * format = "%d\n";
-    /* what is the cause of the compiler warning here ?
-     * how do we fix it? 
-     *
-     * is it reasonable to presume that just because this is a warning that
-     * it is not a big issue?
-     */
-    /* int printf(const char *, ...); */
-    printf(format, i);
-    return EXIT_SUCCESS;
+        int i = 5;
+        /* char * format = "%d\n"; */
+        /* what is the cause of the compiler warning here ?
+         * how do we fix it?
+         *
+         * is it reasonable to presume that just because this is a warning that
+         * it is not a big issue?
+         */
+        /* int printf(const char *, ...); */
+        printf(i);
+        return EXIT_SUCCESS;
 }
