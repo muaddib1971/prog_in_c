d5c34ddc2e77d2b8e0af4c74af18b635a9712f8d
Paul Miller <paul.miller@rmit.edu.au>
2020-01-05 00:56:25 +1100

    modified allocate_nums so it compiles cleanly

--- lectures/Week-07/allocate_nums.c
+++ lectures/Week-07/allocate_nums.c
@@ -1,16 +1,23 @@
-BOOLEAN allocate_ints(int * data, size_t num_ints)
-{
-        data = (int*)malloc(sizeof(int) * num_ints);
-        if(!data)
-        {
-                perror("failed to allocate memory");
-                return FALSE;
-        }
-        return TRUE;
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+typedef enum {
+    FALSE,
+    TRUE
+} BOOLEAN;
+
+BOOLEAN allocate_ints(int* data, size_t num_ints) {
+    data = (int*)malloc(sizeof(int) * num_ints);
+    if (!data) {
+        perror("failed to allocate memory");
+        return FALSE;
+    }
+    return TRUE;
 }
 
-int main(void)
-{
-        int * data;
-        allocate_ints(data, 50);
+int main(void) {
+    int* data;
+    allocate_ints(data, 50);
+    data[5] = 42;
+    return EXIT_SUCCESS;
 }
